fdec090455b7f79ba31672e870df628719595daf
VicenteMdz <vicente.d.llanos@gmail.com>
2024-05-13 12:35:22 -0300

    creacion de las tablas tasks y users en la BD

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"net/http"
 
 	"github.com/VicenteMza/go_gorm_restapi/db"
+	"github.com/VicenteMza/go_gorm_restapi/models"
 	"github.com/VicenteMza/go_gorm_restapi/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	db.DBConnection()
+	//creo las tablas en la base de datos
+	db.DB.AutoMigrate(models.Task{})
+	db.DB.AutoMigrate(models.User{})
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", routes.HomeHandler)
