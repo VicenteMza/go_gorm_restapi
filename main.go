7d73165f4c816bd234a1d98ce073a8f10c9b2c18
VicenteMdz <vicente.d.llanos@gmail.com>
2024-05-12 22:07:36 -0300

    primer commit del backend usando Go

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/VicenteMza/go_gorm_restapi/db"
+	"github.com/VicenteMza/go_gorm_restapi/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	db.DBConnection()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", routes.HomeHandler)
+
+	http.ListenAndServe(":3000", r)
+}
